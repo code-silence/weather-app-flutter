8bdca6f6bb25d6b1d342baa02107c1ece97f78c5
Arnob <arnob8855@gmail.com>
2026-07-20 14:07:36 +0600

    initial page

---
+++ lib/app.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      title: 'Weather App',
+
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+        ),
+        useMaterial3: true,
+      ),
+
+      home: const Scaffold(
+        body: Center(
+          child: Text(
+            'Weather App is born!',
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/services/geocoding_service.dart
+++ lib/services/geocoding_service.dart
@@ -16,19 +16,4 @@ class GeocodingService {
     return response.data!;
   }
 
-  Future<Map<String, dynamic>> reverseGeocode({
-    required double latitude,
-    required double longitude,
-  }) async {
-    final response = await _dio.get<Map<String, dynamic>>(
-      '/reverse',
-      queryParameters: {
-        'latitude': latitude,
-        'longitude': longitude,
-        'count': 1,
-      },
-    );
-
-    return response.data!;
-  }
 }
